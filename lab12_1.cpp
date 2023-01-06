30b74a7678bde323b9ce2e0a6604445c7c7d651f
kittisak101 <kittiisak_b@cmu.ac.th>
2023-01-06 17:42:30 +0700

    ohhhhhhhh

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,29 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    string i;
+    if(x == 0) i = "A";
+    else if(x == 1) i = "B+";
+    else if(x == 2) i = "B";
+    else if(x == 3) i = "C+";
+    else if(x == 4) i = "C";
+    else if(x == 5) i = "D+";
+    else if(x == 6) i = "D";
+    else if(x == 7) i = "F";
+    else if(x == 8) i = "W";
+    cout << "You will get " << i << " in this 261102.";
+}
